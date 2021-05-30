0655da65519327b9a3941fd2cd5e86c0dc88e41e
Michael Santos <mackelldeveloper@gmail.com>
2021-05-30 16:07:36 -0300

    Definição e finalização da página, definição do elementos responsivos.

--- landing-page/js/script.js
+++ landing-page/js/script.js
@@ -2,10 +2,18 @@
 function btnForm() {
     let first_name = document.getElementById('first-name')
     let last_name = document.getElementById('last-name')
-    let femail = document.getElementById('femail')
+    let email = document.getElementById('email')
 
-    if (first_name.value == '' ) {
+    if (first_name.value == '') {
         alert('Campo não informado')
         first_name.focus()
+    } else if (last_name.value == '') {
+        alert('Campo não informado')
+        last_name.focus()
+    } else if (email.value == '') {
+        alert('Campo não informado')
+        email.focus()
+    } else {
+        alert('Obrigado, por falar conosco!')
     }
 }
