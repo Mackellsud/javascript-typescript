7832e387d1b6d17e5c04e5d08b0df922c0f9e87f
Michael Santos <mackelldeveloper@gmail.com>
2021-06-01 01:14:22 -0300

    Alteração, movendo o diretorio pratica para o dir html-css

---
+++ javascript/aula3/aula3.js
@@ -0,0 +1,26 @@
+/* Console.log() 
+
+Retorna o que estamos desenvolvendo no ambiente de desenvolvimento.
+
+Tipos de dados
+
+*/
+
+/* Strings */
+console.log(' Aspas simples ');
+console.log(" Aspas duplas ")
+console.log(` Templates String | Literal `)
+
+/* Number */
+console.log(123)
+console.log(1 + 1)
+console.log(2 * 2)
+console.log(4 - 2)
+console.log(10 / 2)
+
+/* Float */
+console.log(1.2)
+
+/* Boolean */
+console.log(true)
+console.log(false)
