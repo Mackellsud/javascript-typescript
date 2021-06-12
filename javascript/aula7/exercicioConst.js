94f60e30437b6c88620da6dd83b782660e662a9c
Michael Santos <mackelldeveloper@gmail.com>
2021-06-12 05:37:06 -0300

    Constantes

--- javascript/aula7/exercicioConst.js
+++ javascript/aula7/exercicioConst.js
@@ -1,5 +1,11 @@
 const nome = 'Michael';
 const sobrenome = 'Santos';
-const idade = 34;
-const peso = 81;
+const idade = 35;
+const peso = 80;
 const alturaEmM = 1.70;
+let imc = peso / (alturaEmM ** 2)
+let anoNascimento = 2021 - idade
+
+console.log(`${nome + ' ' + sobrenome} tem ${idade} anos, pesa ${peso}kg
+            tem ${alturaEmM.toFixed(2)}m e seu IMC é de ${imc.toFixed(2)}
+            ${nome + ' ' + sobrenome} nasceu em ${anoNascimento}`)
---
+++ javascript/aula8/index.js
@@ -0,0 +1,8 @@
+const nome = 'Usuário';
+const sobrenome = 'Teste';
+const idade = 34;
+const salario = 1.100;
+
+/* O perigo de usar var pois o exemplo acima não exibe erro, apenas sobrescreve a variável (var nome = 'eu', var nome = 'nós' */
+
+
