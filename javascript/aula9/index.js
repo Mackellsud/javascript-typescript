6b231e573d94d1d62efff4724fb9bba132e84f0b
Michael Santos <mackelldeveloper@gmail.com>
2021-06-13 05:31:16 -0300

    Aritmeticos

---
+++ javascript/aula9/index.js
@@ -0,0 +1,14 @@
+/**
+ * Operadores Aritméticos
+ * (+) Adição
+ * (/) Divisão
+ * (*) Multiplicação
+ * (**) Potenciação
+ * (%) Resto
+ * (-) Subtração
+ */ 
+
+const num1 = 5;
+const num2 = 10;
+
+console.log(num1 + num2);
