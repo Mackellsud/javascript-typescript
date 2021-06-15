fe26f8f40af0fa6570c257cd03262bfce9ecce62
Michael Santos <mackelldeveloper@gmail.com>
2021-06-14 22:19:58 -0300

    Operadores Aritmeticos

--- javascript/aula9/index.js
+++ javascript/aula9/index.js
@@ -6,9 +6,24 @@
  * (**) Potenciação
  * (%) Resto
  * (-) Subtração
+ * 
+ * Ordem de precedência
+ * ()
+ * **
+ * * / %
+ * + -
+ * 
  */ 
 
-const num1 = 5;
-const num2 = 10;
+const num1 = 10;
+const num2 = 5;
+
+const num3 = parseInt('5.5');
+const num4 = parseFloat('5.5');
+const num5 = Number('4.8');
 
 console.log(num1 + num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
