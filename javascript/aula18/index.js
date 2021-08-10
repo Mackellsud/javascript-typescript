9cf7c1b5d7554775d7c3b405cfd57016b58f3471
Michael Santos <mackelldeveloper@gmail.com>
2021-08-10 07:15:24 -0300

    Valores primitivos e por referência

--- javascript/aula18/index.js
+++ javascript/aula18/index.js
@@ -2,10 +2,16 @@
 Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - valor
 
 Referência (mutável) - Arrays, Object, Function
-*/
+
+0123
+let b = 'SPFC';
+b[0] = 'AVT';
+console.log(b[0], b);
 
 let a = [1, 2, 3];
-let b = a;
+let b = [...a];
+let c = b;
+
 console.log(a, b)
 
 a.push(5)
@@ -13,3 +19,20 @@ console.log(a, b)
 
 b.pop();
 console.log(a, b)
+
+console.log(c)
+*/
+         
+
+const pessoa = {
+    nome: 'Usuario',
+    lastname: 'Testing'
+};
+
+const c = {...pessoa}
+
+c.nome = 'José';
+c.lastname = 'Oxente';
+
+console.log(c);
+console.log(pessoa);
