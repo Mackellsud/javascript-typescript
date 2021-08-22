388ba02e813b2c64726f5bd21192b0a8e3c31810
Michael Santos <mackelldeveloper@gmail.com>
2021-08-22 06:27:27 -0300

    desafio pegado dados do formulario

---
+++ javascript/aula19/js/script.js
@@ -0,0 +1,36 @@
+function form() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = [];
+
+    function recebeEvento(evento) {
+        evento.preventDefault();
+
+        const name  = form.querySelector('.name');
+        const lastname = form.querySelector('.lastname');
+        const weight = form.querySelector('.weight');
+        const width = form.querySelector('.width');
+
+        pessoas.push({
+            nome: name.value,
+            sobrenome: lastname.value,
+            peso: weight.value,
+            altura: width.value
+        });
+
+        console.log(pessoas)
+
+        resultado.innerHTML += `<p>${name.value} ${lastname.value} ${weight.value} ${width.value}</p>`
+    }
+
+    form.addEventListener('submit', recebeEvento);
+}
+
+form();
+
+/* form.onsubmit = function (evento) {
+        evento.preventDefault();
+        alert('Formulário enviado!')
+        console.log('Formulário enviado!')
+    }; */
